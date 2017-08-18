ce7fc4001b04a1b187f705f0d0b2ee828d25cdfa
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-18 09:22:50 +0900

    コード整形

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const auth = require('http-auth');
 const router = require('./lib/router');
 
 const basic = auth.basic({
-  realm: 'Enter username and password.',
-  file: './users.htpasswd'
+    realm: 'Enter username and password.',
+    file: './users.htpasswd'
 });
 
 const server = http.createServer(basic, (req, res) => {
-  router.route(req, res);
+    router.route(req, res);
 }).on('error', (e) => {
-  console.error('Server Error', e);
+    console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('Client Error', e);
+    console.error('Client Error', e);
 });
 
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+    console.info('Listening on ' + port);
 });
